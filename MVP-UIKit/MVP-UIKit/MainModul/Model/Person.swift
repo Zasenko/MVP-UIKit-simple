e7a571d2263e38dd02e2114f255cbd957f21a069
Zasenko <dmitry.zasenko@gmail.com>
2023-01-03 11:38:47 +0100

    add MainModul

---
+++ MVP-UIKit/MVP-UIKit/MainModul/Model/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  MVP-UIKit
+//
+//  Created by Dmitry Zasenko on 03.01.23.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
---
+++ MVP-UIKit/MVP-UIKit/MainModul/Presenter/MainViewPresenter.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewPresenter.swift
+//  MVP-UIKit
+//
+//  Created by Dmitry Zasenko on 03.01.23.
+//
+
+import Foundation
+
+protocol MainViewProtocol: AnyObject {
+    func setGreeting(greeting: String)
+}
+
+protocol MainViewPresenerProtocol: AnyObject {
+    init(view: MainViewProtocol, person: Person)
+    func showGreeting()
+}
+
+class MainViewPresenter: MainViewPresenerProtocol {
+    
+    let view: MainViewProtocol
+    let person: Person
+    
+    required init(view: MainViewProtocol, person: Person) {
+        self.view = view
+        self.person = person
+    }
+    
+    func showGreeting() {
+        let greeting = person.firstName + " " + person.lastName
+        self.view.setGreeting(greeting: greeting)
+    }
+}
